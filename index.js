e40b5101718ea8029549d5f5ddf955228ae42be2
jck-y <s2200582@student.unklab.ac.id>
2024-01-29 15:51:44 +0800

    ganti date

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const members = require('./members');
 const user = require('./user'); // Perbaiki nama variabel menjadi user
+const moment = require('moment');
 
 const server = http.createServer((req, res) => {
   const path = req.url;
@@ -9,7 +10,6 @@ const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/plain');
     res.write('This is the home page');
     res.end();
-
   } else if (path === '/about') {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
@@ -17,13 +17,15 @@ const server = http.createServer((req, res) => {
       status: 'success',
       message: 'response success',
       description: 'Exercise #03',
-      date: new Date().toISOString(), 
-      data: members 
+      // Tambahkan baris ini untuk menetapkan properti date
+      date: moment().format(),
+      // data: members 
+      
     };
     res.write(JSON.stringify(response));
     res.end();
-
-  } else if (path === "/user") {
+  } // Tambahkan kurung kurawal ini untuk menutup blok else if
+  else if (path === "/user") {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
     const response = {
@@ -34,6 +36,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
-server.listen(3000, () => {
-  console.log('Server berjalan di: http://localhost:3000/');
+const port = 3000;
+const hostname="127.0.0.1";
+server.listen(port, hostname, () => {
+  console.log(`Server berjalan di: http://${hostname}:${port}/`);
 });

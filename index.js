95048507b6b9b989ea9e1b28239d76a944879714
jck-y <s2200582@student.unklab.ac.id>
2024-02-12 08:06:11 +0800

    exercise 3

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const morgan = require('morgan')
+const app = express();
+const port = 3000
+
+// app.use ((req, res, next)=> {
+//     console.log(`${Date.now()} - ${req.ip} - ${req.originalUrl}`)
+//     next();
+// })
+
+app.use(morgan("dev"))
+app.get('/contoh', (req, res)=> 
+    res.json({
+        status: "success",
+        message: "get"
+    })
+);
+
+app.post('/contoh', (req, res)=> 
+    res.json({
+        status: "success",
+        message: "hello world post"
+    })
+);
+// app.post('/contoh', (req, res)=> {
+//     res.send('request dengan method POST')
+// })
+app.put('/contoh', (req, res)=>
+    res.json({
+        status:"success",
+        message: "put"
+    })
+);
+
+app.delete('/contoh', (req, res)=> 
+    res.json({
+        status:"success",
+        message: "delete"
+    })
+);
+
+app.patch('/contoh', (req, res)=> 
+    res.json({
+        status:"success",
+        message: "patch"
+    })
+);
+//parrams
+app.get("/post/:id", (req,res)=>{
+    const id = req.params.id;
+    res.json({
+        status:"success",
+        message: `Artikel ID ke-${id}`
+    })
+})
+
+app.get("/post", (req,res)=>{
+    // const data = req.query
+    const {page, sort}= req.query
+    res.json({
+        status:"success",
+        // message:data.page
+        message: `Page = ${page}, Sort by = ${sort}`
+    })
+})
+
+
+app.listen(port, ()=>console.log(`Server running at http://localhost:${port}`));
